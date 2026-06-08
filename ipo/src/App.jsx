4c9a0283963faa1be4acb880ee6c979ec2876205
André Marques <a14816@oficina.pt>
2026-06-08 09:13:59 +0000

    Parte 2 – Ex2 - React

---
+++ ipo/src/App.jsx
@@ -0,0 +1,46 @@
+import { Routes, Route, Link } from 'react-router-dom';
+function App() {
+  return (
+    <div>
+      {/* Barra de navegação superior em bootstap 4 */}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <Link className="navbar-brand" to="/">IPO</Link>
+          <div className="navbar-nav">
+            <Link className="nav-link" to="/clientes">Clientes</Link>
+            <Link className="nav-link" to="/veiculos">Veículos</Link>
+            <Link className="nav-link" to="/inspecoes">inspecoes</Link>
+          </div>
+        </div>
+      </nav>
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/clientes" element={<ClientesList />} />
+          <Route path="/veiculos" element={<VeiculosList />} />
+          <Route path="/inspecoes" element={<InspecoesList />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+// Estas páginas serão criadas nas próximas etapas
+function Inicio() {
+  return (
+    <div class="jumbotron">
+      <h1 class="text-center">Centro de inspeções de automoveis</h1>
+      <p class="text-center">IPO - ESDS1</p>
+    </div>
+  
+  );
+}
+function ClientesList() {
+  return (<h2>Página de Clientes</h2>);
+}
+function VeiculosList() {
+  return (<h2>Página de Veículos</h2>);
+}
+function InspecoesList() {
+  return (<h2>Página de Inspeções</h2>);
+}
+export default App
